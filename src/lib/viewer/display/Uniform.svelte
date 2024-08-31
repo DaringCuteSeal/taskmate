8eabd9f0d03e364ae27f169455564f858d1a6cc4
Daringcuteseal <daringcuteseal@gmail.com>
2024-08-31 14:30:30 +0700

    feat: uniform viewer component

---
+++ src/lib/viewer/display/Uniform.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+import { getContext } from "svelte";
+const i18n = getContext("i18n");
+
+export let uniform: String | null = "???";
+</script>
+
+<p>
+	<i class="fa-solid fa-shirt"></i>
+	{ $i18n.t("viewer:wear") }
+
+	<span class="viewer-infobox bg-purple">
+		{ uniform ?? "???" }
+	</span>
+
+</p>
