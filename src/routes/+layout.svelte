8413891ba97375bd5463908e5b1764ee0dfd2ef8
Daringcuteseal <daringcuteseal@gmail.com>
2024-08-31 20:01:05 +0700

    refactor: import i18n from correct path on main layout file

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-import getI18nStore from "$lib/locales"
+import getI18nStore from "$lib/locales/i18n"
 import { setContext } from "svelte";
 
 setContext('i18n', getI18nStore());
