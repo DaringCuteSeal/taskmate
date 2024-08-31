80c84962cb4950a44116520199c90e6091890227
Daringcuteseal <daringcuteseal@gmail.com>
2024-08-31 14:30:55 +0700

    feat: uniform bring viewer component

---
+++ src/lib/viewer/display/UniformBring.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+import { getContext } from "svelte";
+const i18n = getContext("i18n");
+
+export let uniform_bring: String | null = "???";
+</script>
+
+<p>
+	<i class="fa-solid fa-bag-shopping"></i>
+	{ $i18n.t("viewer:bring_uniform") }
+
+	<span class="viewer-infobox bg-pink">
+		{ uniform_bring ?? "???" }
+	</span>
+</p>
