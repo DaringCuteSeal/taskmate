929ee1f11fa7d191c133c95482f80ef53cd5eadf
DaringCuteSeal <daringcuteseal@gmail.com>
2026-02-04 08:22:40 +0700

    redirect to viewer on root

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML>
+<html lang="en">
+	<head>
+		<meta http-equiv="refresh" content="0; url=/viewer" />
+		<meta charset="utf-8" />
+		<link rel="icon" href="%sveltekit.assets%/favicon.png" />
+		<link rel="stylesheet" href="%sveltekit.assets%/style/common.css" />
+	</head>
+</html>
